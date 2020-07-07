c56f6f8ed3a20bbdeb077d72b343731492b74558
Tyler Coy <tylercoy@utexas.edu>
2020-07-06 22:23:25 -0500

    Change example app optimization level

--- example.mk
+++ example.mk
@@ -49,8 +49,8 @@ OPTFLAGS := \
 	-finline-functions \
 
 TGT_INCDIRS  := . example example/hal
-TGT_CFLAGS   := -Og -g $(ARCHFLAGS) $(OPTFLAGS) $(WARNFLAGS) -std=gnu11
-TGT_CXXFLAGS := -Og -g $(ARCHFLAGS) $(OPTFLAGS) $(WARNFLAGS) -std=gnu++17 \
+TGT_CFLAGS   := -Os -g $(ARCHFLAGS) $(OPTFLAGS) $(WARNFLAGS) -std=gnu18
+TGT_CXXFLAGS := -Os -g $(ARCHFLAGS) $(OPTFLAGS) $(WARNFLAGS) -std=gnu++17 \
 	-fno-exceptions -fno-rtti -Wno-register
 TGT_LDLIBS   := -lm -lc -lgcc
 TGT_LDFLAGS  := $(ARCHFLAGS) \
