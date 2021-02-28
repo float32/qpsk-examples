7a30214c7ae2556a7b4c5017b87ff88de87ee5fd
Tyler Coy <tylercoy@utexas.edu>
2021-02-27 22:34:20 -0600

    Make pointer returned from Decoder::block_data() const

--- unit_tests/test_decoder.cpp
+++ unit_tests/test_decoder.cpp
@@ -199,7 +199,7 @@ public:
                 }
                 else if (result == RESULT_BLOCK_COMPLETE)
                 {
-                    uint32_t* block = qpsk_.block_data();
+                    const uint32_t* block = qpsk_.block_data();
                     for (uint32_t i = 0; i < kBlockSize / 4; i++)
                     {
                         data.push_back(block[i] >>  0);
